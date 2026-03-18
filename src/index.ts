a3beb0b5195f9cd2695913deff6454e4e7b8926a
JC Vizcarra <jvizcar@emory.edu>
2026-03-18 12:36:25 -0400

    added missing imports, adding vite-plugin-lib-inject-css

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 // Components
 export { Button } from './components/Button/Button'
 export type { ButtonProps } from './components/Button/Button'
+
+export { DsaAuthManager } from './components/DsaAuthManager/DsaAuthManager'
+export type { DsaAuthManagerProps } from './components/DsaAuthManager/DsaAuthManager'
+export { dsaAuthStore } from './auth/DsaAuthStore'
+export type { DsaAuthStatus, DsaAuthConfig, DsaUserInfo } from './auth/types'
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        libInjectCss(),
         dts({
             insertTypesEntry: true,
             include: ['src'],
@@ -40,9 +42,6 @@ export default defineConfig({
         emptyOutDir: true,
         cssCodeSplit: false,
         minify: 'esbuild',
-    },
-    esbuild: {
-        logOverride: { 'css-syntax-error': 'silent' },
     },
 })
 
